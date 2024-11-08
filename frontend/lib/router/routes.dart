817344dd3f57258d85a30d1af4930fec5fac156d
Adid Rahman <adidra10@gmail.com>
2024-11-08 10:50:48 +0600

    refactor: Update routes and navigation drawer
    This commit refactors the routes and navigation drawer in the `routes.dart` file. It updates the titles and paths of the navigation drawer items, adds new routes for different pages such as profile, about, settings, login, signup, and welcome, and updates the route builders to display the corresponding text for each route.

--- frontend/lib/router/routes.dart
+++ frontend/lib/router/routes.dart
@@ -10,20 +10,70 @@ final GoRouter router = GoRouter(
           children: [
             NavigationDrawer(children: [
               ListTile(
-                title: const Text('Home'),
+                title: const Text('Community'),
                 onTap: () => context.go('/'),
-              )
+              ),
+              ListTile(
+                title: const Text('profile'),
+                onTap: () => context.go('/jonogon/1'),
+              ),
+              ListTile(
+                title: const Text('About Us'),
+                onTap: () => context.go('/about'),
+              ),
+              ListTile(
+                title: const Text('Settings'),
+                onTap: () => context.go('/settings'),
+              ),
             ]),
             child,
           ],
         ),
       ),
       routes: <RouteBase>[
-        GoRoute(path: '/jonogon/:id', builder: (context, state) {
-          final id = state.pathParameters["id"];
-          return Text("Jonogon $id");
-        }),
+        GoRoute(
+            path: '/jonogon/:id',
+            builder: (context, state) {
+              final id = state.pathParameters["id"];
+              return Text("Jonogon $id");
+            }),
         GoRoute(path: '/', builder: (context, state) => const Text("Home")),
+        GoRoute(
+            path: '/initiative/:id',
+            builder: (context, state) {
+              final id = state.pathParameters["id"];
+              return Text("Initiative $id");
+            }),
+        GoRoute(
+            path: '/take-initiative',
+            builder: (context, state) {
+              return const Text("take-initiative");
+            }),
+        GoRoute(
+            path: '/about',
+            builder: (context, state) {
+              return const Text("about");
+            }),
+        GoRoute(
+            path: '/settings',
+            builder: (context, state) {
+              return const Text("settings");
+            }),
+        GoRoute(
+            path: '/login',
+            builder: (context, state) {
+              return const Text("login");
+            }),
+        GoRoute(
+            path: '/signup',
+            builder: (context, state) {
+              return const Text("signup',");
+            }),
+        GoRoute(
+            path: '/welcome',
+            builder: (context, state) {
+              return const Text("welcome");
+            }),
       ],
     )
   ],
