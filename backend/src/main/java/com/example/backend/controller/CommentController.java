26bab8b6ca6fa690e5c74199b72b5dbfe535d7af
Zarif Muhtasim <2502.zarif@gmail.com>
2024-11-08 11:53:47 +0600

    fix: error

--- backend/src/main/java/com/example/backend/controller/CommentController.java
+++ backend/src/main/java/com/example/backend/controller/CommentController.java
@@ -1,5 +1,8 @@
 package com.example.backend.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CommentController {
     
 }
--- backend/src/main/java/com/example/backend/entity/Initiative.java
+++ backend/src/main/java/com/example/backend/entity/Initiative.java
@@ -6,17 +6,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.annotation.Nonnull;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -32,15 +22,11 @@ public class Initiative {
     private Long id;
 
     @Nonnull
-    @Column(name = "jonogon_id")
-    private Long jonogonId;
-
-    @Nonnull
     @Column(name = "description")
     private String decscription;
 
     @Nonnull
-    @Column(name = "createdAt")
+    @Column(name = "created_at")
     private LocalDate createdAt;
 
     @Nonnull
